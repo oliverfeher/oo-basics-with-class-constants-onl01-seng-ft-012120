e4227e97376e51c1450634a59596fdb5ee03ed6d
Oliver Feher <olcsee@gmail.com>
2020-01-28 05:06:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,8 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    if BRANDS[0..-1] != brand
-      BRANDS.push(brand)
+    if BRANDS.!include?(brand)
+      BRANDS << brand
     end
   end
 
